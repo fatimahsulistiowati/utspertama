ed49c9e8fd4a6a8b45a8a038158d59db6b726ed2
fatimahsulistiowati <fatimahsulistio11@gmail.com>
2023-05-11 22:17:26 +0700

    uts

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import pengguna.User;
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
+        
+        Mhs mhs = new Mhs("username", "password");
 
+        mhs.setNama("Fatimah Sulistiowati");
+        mhs.setNim("G.111.20.0030");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mahasiswa: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("USERNAME: " + mhs.getUsername());
+        System.out.println("PASSWORD: " + mhs.getPassword());
     }
 }
